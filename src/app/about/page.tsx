19e39eeddd41c8600ebf11e49ad0b0020a0e6405
Skrylleur <106743803+Skrylleur@users.noreply.github.com>
2025-05-05 21:00:58 +0200

    Ajout de fichiers dans about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,6 +19,27 @@ export default function AboutPage() {
           >
             Télécharger mon CV
           </a>
+          <a
+            href="documents/Synthèse.pdf"
+            download
+            className="inline-block px-6 py-3 border border-[#0c1c3c] text-[#0c1c3c] rounded-md font-medium hover:bg-[#f0c98e] transition"
+          >
+            Télécharger mon tableau de synthèse
+          </a>
+          <a
+            href="documents/veille_technologique_dotnet_2025_complete.pdf"
+            download
+            className="inline-block px-6 py-3 border border-[#0c1c3c] text-[#0c1c3c] rounded-md font-medium hover:bg-[#f0c98e] transition"
+          >
+            Télécharger ma veille technologique
+          </a>
+          <a
+            href="documents/veille_juridique_azure_ad_2025.pdf"
+            download
+            className="inline-block px-6 py-3 border border-[#0c1c3c] text-[#0c1c3c] rounded-md font-medium hover:bg-[#f0c98e] transition"
+          >
+            Télécharger ma veille juridique
+          </a>
         </div>
       </div>
     </main>
